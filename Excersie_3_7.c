91eff5ceaa41395286c023f8aaedadaf8068a911
Oscar Diaz <oscar.dh@unitec.edu>
2018-02-24 21:34:48 -0600

    Average

--- Excersie_3_7.c
+++ Excersie_3_7.c
@@ -6,6 +6,10 @@
 int main(int argc, char const *argv[]) {
     double startTime, endTime, totalTime;
     int myRank, commSize;
+    
+    for(int i=0; i < argc; ++i){   
+        printf("Argument %d : %s\n", i, argv[i]);
+    }
 
     MPI_Init(NULL, NULL);
         MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
@@ -27,17 +31,24 @@ int main(int argc, char const *argv[]) {
         } else {
             double endTimeA = 0;
             double endTimeB = 0;
+            double average = 0;
             int A, B;
+            
+            printf("-------------------------------------------------------------\n");
             for (A = 1, B = 2; A < commSize - 1; A += 2, B += 2) {
                 MPI_Recv(&endTimeA, 1, MPI_DOUBLE, A, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 MPI_Recv(&endTimeB, 1, MPI_DOUBLE, B, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
                 totalTime = endTimeA + endTimeB;
+                average += totalTime;
                 printf("Ping From =>\t%d\tTo =>\t%d\tTime =>\t%f\n", A, B, endTimeA);
                 printf("Pong From =>\t%d\tTo =>\t%d\tTime =>\t%f\n", B, A, endTimeB);
                 printf("Total Time =>\t%f\n", totalTime);
                 printf("-------------------------------------------------------------\n");
             }
+            
+            average /= (commSize/ 2);
+            printf("Ping-Pong Average =>%f\t", average);
         }
     
     MPI_Finalize();
