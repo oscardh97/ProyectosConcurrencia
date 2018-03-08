764a6b0aee596f589bd351d4cabe683906152e9e
Oscar Diaz <oscar.dh@unitec.edu>
2018-03-07 20:20:54 -0600

    Output changes

--- Pthread/trapezoid.c
+++ Pthread/trapezoid.c
@@ -3,11 +3,11 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-int thread_count, n, local_n;
+int threadCount, n, localN;
 double a, b, h, total;
 pthread_mutex_t mutex;
 
-void *work(void *rank);
+void *getLocalInt(void *rank);
 double Trap(double leftEndpt, double rightEndpt, int trapCount, double baseLen);
 double f(double x);
 
@@ -16,8 +16,7 @@ double f(double x)
     return x * x * x;
 }
 
-double Trap(double leftEndpt, double rightEndpt, int trapCount, double baseLen)
-{
+double Trap(double leftEndpt, double rightEndpt, int trapCount, double baseLen) {
     double estimate, x;
 
     estimate = (f(leftEndpt) + f(rightEndpt)) / 2;
@@ -33,15 +32,14 @@ double Trap(double leftEndpt, double rightEndpt, int trapCount, double baseLen)
     return estimate;
 }
 
-void *work(void *rank)
-{
+void *getLocalInt(void *rank) {
     double leftEndpt, rightEndpt, estimate;
     long my_rank = (long)rank;
 
-    leftEndpt = a + my_rank * local_n * h;
-    rightEndpt = leftEndpt + local_n * h;
+    leftEndpt = a + my_rank * localN * h;
+    rightEndpt = leftEndpt + localN * h;
 
-    estimate = Trap(leftEndpt, rightEndpt, local_n, h);
+    estimate = Trap(leftEndpt, rightEndpt, localN, h);
     pthread_mutex_lock(&mutex);
     total += estimate;
     pthread_mutex_unlock(&mutex);
@@ -49,35 +47,31 @@ void *work(void *rank)
     return NULL;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     pthread_t *master_thread;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Numero de threads: %s <number of threads>\n", argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "Total threads: %s <number of threads>\n", argv[0]);
         exit(0);
     }
 
-    thread_count = strtol(argv[1], NULL, 10);
+    threadCount = strtol(argv[1], NULL, 10);
     a = 0.0, b = 50000.0, n = 1024;
     h = (b - a) / n;
-    local_n = n / thread_count;
+    localN = n / threadCount;
 
-    master_thread = malloc(thread_count * sizeof(pthread_t));
+    master_thread = malloc(threadCount * sizeof(pthread_t));
     pthread_mutex_init(&mutex, NULL);
 
-    for (long i = 0; i < thread_count; i++)
-    {
-        pthread_create(&master_thread[i], NULL, work, (void *)i);
+    for (long i = 0; i < threadCount; i++) {
+        pthread_create(&master_thread[i], NULL, getLocalInt, (void *)i);
     }
 
-    for (long i = 0; i < thread_count; i++)
-    {
+    for (long i = 0; i < threadCount; i++) {
         pthread_join(master_thread[i], NULL);
     }
 
-    printf("With n = %d trapezoids, our estimate\n", n);
+    printf("With %d trapezoids\n", n);
     printf("of the integral from %f to %f = %19.15e\n", a, b, total);
 
     pthread_mutex_destroy(&mutex);
